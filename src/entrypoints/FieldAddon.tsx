6ce00e45358e5355f04a423630b9715915bedd10
Daniel <daniel@setsnail.com>
2024-05-13 10:50:33 +0200

    made sanitizer work

--- src/entrypoints/FieldAddon.tsx
+++ src/entrypoints/FieldAddon.tsx
@@ -1,14 +1,24 @@
 import type { RenderFieldExtensionCtx } from 'datocms-plugin-sdk';
 import { Canvas } from 'datocms-react-ui';
 import { defaultConfig, type Config } from '../utils/config';
+import { useEffect } from 'react';
+import { sanitize } from '../utils/xss';
 
 export const FieldAddon = ({ ctx }: { ctx: RenderFieldExtensionCtx }) => {
-	// const config: Config = {
-	// 	...defaultConfig,
-	// 	...ctx.plugin.attributes.parameters,
-	// 	...ctx.parameters,
-	// };
+	useEffect(() => {
+		const config: Config = {
+			...defaultConfig,
+			...ctx.plugin.attributes.parameters,
+			...ctx.parameters,
+		};
 
+		const value = ctx.formValues[ctx.fieldPath];
+		const parsed = sanitize(value as string, config);
+
+		if (parsed !== value) {
+			ctx.setFieldValue(ctx.fieldPath, parsed);
+		}
+	}, [ctx.formValues[ctx.fieldPath]]);
 
 	return (
 		<Canvas ctx={ctx}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import { type Field, connect } from 'datocms-plugin-sdk';
+import { connect } from 'datocms-plugin-sdk';
 import { render } from './utils/render';
 import 'datocms-react-ui/styles.css';
 import './styles.css';
 import { FieldConfigScreen } from './entrypoints/FieldConfigScreen';
 import { FieldAddon } from './entrypoints/FieldAddon';
 import { PluginConfigScreen } from './entrypoints/PluginConfigScreen';
-import { defaultConfig } from './utils/config';
-import { sanitize } from './utils/xss';
 
 connect({
 	manualFieldExtensions() {
@@ -24,37 +22,9 @@ connect({
 		return render(<PluginConfigScreen ctx={ctx} />);
 	},
 	renderFieldExtension(_fieldExtensionId, ctx) {
-		render(<FieldAddon ctx={ctx} />);
+		return render(<FieldAddon ctx={ctx} />);
 	},
 	renderManualFieldExtensionConfigScreen(_fieldExtensionId, ctx) {
-		render(<FieldConfigScreen ctx={ctx} />);
-	},
-	onBeforeItemUpsert(item, ctx) {
-		if (!ctx.fields || !item.data.attributes) return true;
-		const fields = Object.values(ctx.fields).filter(field =>
-			field?.attributes.appearance.addons.some(addon => addon.id === ctx.plugin.id)
-		) as Field[];
-
-		const keys = Object.keys(item.data.attributes);
-		for (const key of keys) {
-			const field = fields.find(field => field.attributes.api_key === key);
-			if (!field) continue;
-
-			const config = {
-				...ctx.plugin.attributes.parameters,
-				...field.attributes.appearance.addons.find(addon => addon.id === ctx.plugin.id)?.parameters,
-				...defaultConfig,
-			};
-
-			try {
-				const parsed = sanitize(item.data.attributes[key] as string, config);
-				item.data.attributes[key] = parsed;
-			} catch (e) {
-				console.error(e);
-				ctx.notice(`Error sanitizing field ${field.attributes.label}`);
-			}
-		}
-
-		return true;
+		return render(<FieldConfigScreen ctx={ctx} />);
 	},
 });
